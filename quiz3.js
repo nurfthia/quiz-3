3e4ab51765e13d325b55b34b8fd03983d4d8e2cf
unknown <nurfthia>
2023-04-11 14:58:52 +0800

    quiz3.js

--- quiz3.js
+++ quiz3.js
@@ -4,25 +4,70 @@ const port = 3000
 
 app.use(express.json())
 
-app.post('/login',(req,res) => {
+app.post('/login', (req, res) => {
     console.log(req.body)
 
-    res.send('Login"')
-})
+    let result = login(req.body.username, req.body.password)
 
+    res.send(result)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/', (req, res) => {
-    res.send('Bye Bye UTeM!')
-  })
+app.get('/bye', (req, res) => {
+    res.send('Bubyeee Semua!')
+})
 
-  app.get('/', (req, res) => {
-    res.send('Account Created!')
+app.post('/register', (req, res) => {
+  res.send('Account Created.')
+})
+
+app.post('/register', (req, res) => {
+  let result = register(
+    req.body.username, 
+    req.body.password, 
+    req.body.name, 
+    req.body.email
+    )
+    res.send(Result)
+})
+
+let dbUsers = [
+  {
+    username: "fathia",
+    password: "060899",
+    name: "Fathia",
+    email: "fathia@utem.edu.my"
+  }
+]
+
+  function login(username, password) {
+    let matchUser = dbUsers.find(
+        user => user.username == username      //=> what to do with user
+    )
+    if (!matchUser) return "User not found!"
+    if (matchUser.password == password) 
+    {
+        return matchUser
+    } else
+    {
+        return "Invalid password"
+    }
+    }
+
+function register(requsername, reqpassword, reqname, reqemail) {
+  dbUsers.push({
+      username: requsername,
+      password: reqpassword,
+      name: reqname,
+      email: reqemail
   })
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+//try login
